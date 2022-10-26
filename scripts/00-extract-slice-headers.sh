3458965f0977b8a084fe92cd4dc5e32020aa10a7
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2022-10-26 16:09:14 -0700

    Update repo

--- scripts/00-extract-slice-headers.sh
+++ scripts/00-extract-slice-headers.sh
@@ -7,17 +7,17 @@
 #mkdir I9N ; unzip input/I09N_33RR20070322_ct1.zip ; mv *csv I9N
  
 #strip off header lines that aren't needed
-for item in P16S/* ; do 
+for item in I8S/* ; do 
 	
-	outfile=P16S/`basename $item .csv`.stripped.csv
-	tail -n+37 $item | grep -v "END" | grep -v "DBAR" > $outfile
+	outfile=I8S/`basename $item .csv`.stripped.csv
+	tail -n+51 $item | grep -v "END" | grep -v "DBAR" > $outfile
 
 done
 
 #slice off header lines that aren't needed
-for item in P16N/* ; do 
+for item in I9N/* ; do 
 
-	outfile=P16N/`basename $item .csv`.stripped.csv	
-	tail -n+53 $item | grep -v "END" | grep -v "DBAR" > $outfile
+	outfile=I9N/`basename $item .csv`.stripped.csv	
+	tail -n+56 $item | grep -v "END" | grep -v "DBAR" > $outfile
 
 done
